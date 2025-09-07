0c9d98948fcc300dec533f2fa78d69596dc9c1c8
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-09-07 10:53:46 +0000

    init

---
+++ src/main/kotlin/is/hi/hbv501g/repx/RepXApplication.kt
@@ -0,0 +1,11 @@
+package `is`.hi.hbv501g.repx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RepXApplication
+
+fun main(args: Array<String>) {
+    runApplication<RepXApplication>(*args)
+}
---
+++ src/test/kotlin/is/hi/hbv501g/repx/RepXApplicationTests.kt
@@ -0,0 +1,13 @@
+package `is`.hi.hbv501g.repx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RepXApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
